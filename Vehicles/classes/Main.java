7e352496e16a78987fdf1d0e115e75ad2c5083c2
koncho1 <xvx2402@gmail.com>
2024-08-29 09:03:39 +0200

    collections

--- Vehicles/classes/Main.java
+++ Vehicles/classes/Main.java
@@ -5,16 +5,33 @@ import exceptions.NoEngineVolumeException;
 import exceptions.NoTopSpeedException;
 import exceptions.NoWheelsException;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws NoWheelsException, NoTopSpeedException {
         Engine engine=new Engine("gasoline",1.5f);
         Engine truckEngine=new Engine("diesel",6f);
+        HashMap map=new HashMap<Integer,Engine>();
+        map.put(1,engine);
+        map.put(2,truckEngine);
         Radiator radiator= new Radiator(60);
+        Radiator radiatorX= new Radiator(90);
+        Radiator radiatorY= new Radiator(40);
+        HashSet set= new HashSet<Radiator>();
+        set.add(radiator);
+        set.add(radiatorX);
+        set.add(radiatorY);
         RaceCar car=new RaceCar(4,0,300);
-        Bus bus=new Bus(6,2,40,10000);
+        LinkedList raceCars=new LinkedList<RaceCar>();
+        raceCars.add(car);
+        Bus bus1=new Bus(6,2,40,10000);
+        Bus bus2=new Bus(6,4,50,30000);
         Motorcycle motorcycle=new Motorcycle(2,0,engine,radiator,300);
+        ArrayDeque motorcycles=new ArrayDeque<Motorcycle>();
+        motorcycles.add(motorcycle);
+        ArrayList busses=new ArrayList<Bus>();
+        busses.add(bus1);
+        busses.add(bus2);
         int in=0;
         try(Scanner scanner=new Scanner(System.in)) {
             System.out.println("Enter number of doors");
